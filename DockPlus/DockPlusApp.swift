99b9c6ad6eb5d48b85253be6ef06dd592e974fce
Jtehuintle <juanmiguel102938@gmail.com>
2025-08-13 22:07:49 -0700

    Initial Commit

---
+++ DockPlus/DockPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockPlusApp.swift
+//  DockPlus
+//
+//  Created by Juan Tehuintle Temor on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct DockPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DockPlusTests/DockPlusTests.swift
@@ -0,0 +1,17 @@
+//
+//  DockPlusTests.swift
+//  DockPlusTests
+//
+//  Created by Juan Tehuintle Temor on 8/13/25.
+//
+
+import Testing
+@testable import DockPlus
+
+struct DockPlusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
